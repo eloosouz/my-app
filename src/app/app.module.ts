017f2d33c7b6ece2c9b6a177102672aa28494be4
Elo Souza <elosouz.dev@gmail.com>
2024-05-27 12:37:10 -0400

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { FilhoComponent } from './components/filho/filho.component';
 import { ItemAddComponent } from './item-add/item-add.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
 import { FormsModule } from '@angular/forms';
+import { Datepicker2Component } from './components/avancados-video/datepicker2.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
@@ -17,14 +24,23 @@ import { FormsModule } from '@angular/forms';
     PaiComponent,
     FilhoComponent,
     ItemAddComponent,
-    ItemListComponent
+    ItemListComponent,
+    Datepicker2Component,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule
+  
+
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
-    
+    FormsModule,
+    BrowserAnimationsModule,
+
     
   ],
   providers: [],
---
+++ src/app/components/avancados-video/datepicker2.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component ({
+    selector: 'app-datepicker2',
+    templateUrl: './datepicker2.component.html'
+})
+
+export class Datepicker2Component implements OnInit {
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
